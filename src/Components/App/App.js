c608793aec0c55be86f9a6f9a53f580a1724464a
switch2512 <gietzen.cameron@gmail.com>
2022-04-05 09:09:54 -0700

    Update containing component did mount

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,6 +21,10 @@ class App extends React.Component {
     this.search = this.search.bind(this);
   }
 
+  componentDidMount(){
+    alert("Unfortunately, due to a change Spotify made, only whitelisted accounts can use this react app")
+  }
+
   addTrack(track) {
     let tracks = this.state.playlistTracks;
     if (tracks.find(savedTrack => savedTrack.id === track.id)) {
